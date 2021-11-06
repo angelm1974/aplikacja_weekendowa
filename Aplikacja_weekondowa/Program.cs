8a535bcb2fc71048da82e52f6e1647e319c19e32
angelm1974 <18185521+angelm1974@users.noreply.github.com>
2021-11-06 11:41:38 +0100

    Start aplikacji

---
+++ Aplikacja_weekondowa/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplikacja_weekondowa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
