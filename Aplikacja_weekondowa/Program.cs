7fde7d1c022ebb0efc9b053a03af326922c8aa9b
Wersjon <vversjon@gmail.com>
2021-11-06 12:10:30 +0100

    Add basic adding function

--- Aplikacja_weekondowa/Program.cs
+++ Aplikacja_weekondowa/Program.cs
@@ -8,6 +8,11 @@ namespace Aplikacja_weekondowa
 {
     class Program
     {
+        static int add(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello");
